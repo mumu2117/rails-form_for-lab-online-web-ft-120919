553cbaef3b4b1ce7acc2cbc4c5b90958a06b4c42
Jennifer Miller <jmillerarcher@me.com>
2020-03-16 01:12:52 +0000

    Automatically backed up by Learn

--- spec/features/school_class_spec.rb
+++ spec/features/school_class_spec.rb
@@ -1,41 +1,41 @@
-# require 'rails_helper'
+require 'rails_helper'
 
 
-# describe 'form page' do
-#   it 'form renders with the new action' do
-#     visit new_school_class_path
-#     expect(page).to have_content("School Class Form")
-#   end
+describe 'form page' do
+  it 'form renders with the new action' do
+    visit new_school_class_path
+    expect(page).to have_content("School Class Form")
+  end
 
-#   it 'new form submits content and renders form content' do
-#     visit new_school_class_path
+  it 'new form submits content and renders form content' do
+    visit new_school_class_path
 
-#     fill_in 'school_class_title', with: "Software Engineering"
-#     fill_in 'school_class_room_number', with: 10
+    fill_in 'school_class_title', with: "Software Engineering"
+    fill_in 'school_class_room_number', with: 10
 
-#     click_on "Create School class"
+    click_on "Create School class"
 
-#     expect(page).to have_content("Software Engineering")
-#   end
+    expect(page).to have_content("Software Engineering")
+  end
 
-#   it 'edit form submits content and renders form content' do
-#     @edit_school_class = SchoolClass.create(title: "Computer Science", room_number: 5)
+  it 'edit form submits content and renders form content' do
+    @edit_school_class = SchoolClass.create(title: "Computer Science", room_number: 5)
 
-#     visit edit_school_class_path(@edit_school_class)
+    visit edit_school_class_path(@edit_school_class)
 
-#     fill_in 'school_class_title', with: "Risk Analysis"
-#     fill_in 'school_class_room_number', with: 10
+    fill_in 'school_class_title', with: "Risk Analysis"
+    fill_in 'school_class_room_number', with: 10
 
-#     click_on "Update School class"
+    click_on "Update School class"
 
-#     expect(page).to have_content("Risk Analysis")
-#   end
+    expect(page).to have_content("Risk Analysis")
+  end
 
-#   it 'submitted new form creates a record in the database' do
-#     visit new_school_class_path
+  it 'submitted new form creates a record in the database' do
+    visit new_school_class_path
 
-#     fill_in 'school_class_title', with: "Sabermetrics"
-#     fill_in 'school_class_room_number', with: 42
+    fill_in 'school_class_title', with: "Sabermetrics"
+    fill_in 'school_class_room_number', with: 42
 
 #     click_on "Create School class"
 
